23c8adbff1d4fbe69aa40a04db56e822981659b2
salwyn <salwynmathew13@gmail.com>
2023-04-05 19:30:30 +0530

    reverse a string

---
+++ reverseString.js
@@ -0,0 +1,12 @@
+// Reverse a string in JS
+// how to take input in js
+// how to index in js
+// and finally print it
+
+var str = "Salwyn";
+len = str.length;
+var list = [];
+for (let i=len; i>=0; i--){
+    list.push(str[i]);
+}
+console.log(list.join(""));
